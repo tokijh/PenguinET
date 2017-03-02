9aef43a35520558bd98f06b7a33636fa1d25400d
timejp <tokijh@naver.com>
2017-03-02 15:26:34 +0900

    logger.h 중복 사용 가능하도록 변경

--- app/src/main/cpp/logger.h
+++ app/src/main/cpp/logger.h
@@ -9,9 +9,10 @@
 
 static const bool debug = true;
 
+#ifndef LOG_TAG
 #define  LOG_TAG    "mainJNI"
 #define  LOGI(...)  if(debug)__android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  if(debug)__android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-
+#endif
 
 #endif //PENGUINET_LOGGER_H
